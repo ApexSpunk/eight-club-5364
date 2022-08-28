70f6edcfc9693d2e55b117c8e476f47b65c81867
Chandan <indspunk@gmail.com>
2022-08-28 08:19:09 +0530

    updated

---
+++ components/footer.js
@@ -0,0 +1,76 @@
+function Footer() {
+    return (`<div class="footer">
+    <div class="footerFirst">
+        <div class="footerFirstMenu">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/airtable-1482122-1254387.png" alt="">
+            <p>Products</p>
+            <p>Solutions</p>
+            <p>Pricing</p>
+            <p>Enterprise</p>
+            <p>Contact Sales</p>
+        </div>
+        <div>
+            <img src="./img/Screenshot 2022-08-27 at 6.50.31 PM.png" alt="">
+        </div>
+    </div>
+    <hr>
+    <div class="footerSecond">
+        <div>
+            <p>Popular Templates</p>
+            <p>Content Calander</p>
+            <p>Product Roadmap</p>
+            <p>Inventory Tracking</p>
+            <p>Event Planning</p>
+            <p>Show More</p>
+        </div>
+        <div>
+            <p>Popular Extension</p>
+            <p>Miro</p>
+            <p>Jira</p>
+            <p>ClearBit</p>
+            <p>Loom</p>
+            <p>Form Stack</p>
+            <p>Show More</p>
+        </div>
+        <div>
+            <p>Popular Solutions</p>
+            <p>Marketing</p>
+            <p>Product Operations</p>
+            <p>Human Resources</p>
+            <p>Sales</p>
+            <p>Operations</p>
+            <p>Show More</p>
+        </div>
+        <div>
+            <p>Learn More</p>
+            <p>Community</p>
+            <p>Support</p>
+            <p>Articles</p>
+            <p>Guides</p>
+            <p>Customer Stories</p>
+        </div>
+        <div>
+            <p>Company</p>
+            <p>About</p>
+            <p>Carriers</p>
+            <p>Blog</p>
+            <p>Status</p>
+            <p>NewsRoom</p>
+        </div>
+    </div>
+</div>
+<div class="footerThird">
+    <div class="footerLastMenu">
+        <p>Security</p>
+        <p>Sitemap</p>
+        <p>API</p>
+        <p>Privacy</p>
+        <p>Terms</p>
+    </div>
+    <div>
+        <img src="./img/Screenshot 2022-08-27 at 7.10.56 PM.png" alt="">
+    </div>
+</div>`)
+}
+
+export default Footer
